344a5a74779cbb350c97b9ae584a4ac0ec0a0458
BCleveland <btcleveland@gmail.com>
2025-12-11 14:49:19 -0600

    Automatic firing and popup

--- tiger-quest/Assets/Scripts/trashboy.gd
+++ tiger-quest/Assets/Scripts/trashboy.gd
@@ -1,33 +1,49 @@
 extends Node2D
 
 @export var ProjectilePrefab: PackedScene
-@export var SwitchTime: float = 2
+@export var RaiseTime: float = 0.5
+@export var LowerSpriteHeight: float = 120
+@export var InitialFireDelay: float = 0.5
+@export var FireTime: float = 3.0
 
 @onready var anim: AnimationPlayer = $AnimationPlayer
+@onready var faceSprite: Sprite2D = $Scalar/Face
+@onready var hatSprite: Sprite2D = $Scalar/Hat
 
-var switch_timer: float = 0
-var is_raised: bool = false
+var raisePercent: float = 0
+var fireTimer: float = 0
 
 var overlappingBody: Node2D
 
 func _ready() -> void:
-	anim.play("stay_low")
+	pass
 
 func _process(delta: float) -> void:
-	switch_timer += delta
-	if(switch_timer > SwitchTime):
-		switch_timer = 0
-		is_raised = !is_raised
-		if(is_raised):
-			anim.play("raise")
+	if overlappingBody != null:
+		fireTimer -= delta
+		if(fireTimer <= 0):
+			fireProjectile()
+		if(raisePercent < 1):
+			raisePercent += 1/RaiseTime*delta
+		if overlappingBody.position.x > position.x:
+			faceSprite.scale.x = -1
 		else:
-			anim.play("lower")
+			faceSprite.scale.x = 1
+	else:
+		if(fireTimer > InitialFireDelay):
+			fireTimer -= delta
+		if(raisePercent > 0):
+			raisePercent -= 1/RaiseTime*delta
+	faceSprite.position.y = lerpf(LowerSpriteHeight, 0, raisePercent)
+	hatSprite.position.y = lerpf(LowerSpriteHeight, 0, raisePercent)
 
 func _on_monitor_zone_body_entered(body: Node2D) -> void:
 	if(overlappingBody != null):
 		printerr("Trashboy overlapped with second body")
 		return
 	overlappingBody = body
+	if(fireTimer < InitialFireDelay):
+		fireTimer = InitialFireDelay
 	#begin popping out animation
 	#after it finishes, throw projectile
 	#wait
@@ -39,6 +55,7 @@ func fireProjectile():
 		projectile.position = position
 		get_parent().call_deferred("add_child", projectile)
 		projectile.initialize(overlappingBody.position)
+		fireTimer = FireTime
 
 
 func _on_monitor_zone_body_exited(body: Node2D) -> void:
